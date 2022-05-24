421295023295bf6ba8c10e407f8e771c7f023e90
MDallah <muhammaddallah093@gmail.com>
2022-05-24 09:13:38 +0200

    Add magicmouse.js

--- src/components/Portfolio.ts
+++ src/components/Portfolio.ts
@@ -1,4 +1,5 @@
 import { defineComponent } from 'vue';
+import { magicMouse } from "magicmouse.js";
 
 export default defineComponent({
     name: 'Portfolio',
@@ -9,5 +10,27 @@ export default defineComponent({
         window.document.body.style.backgroundColor = "#b08b78";
         window.document.body.style.width = "100%";
         window.document.body.style.overflowX = "hidden";
+
+        // magicMouse .................................
+        const options: any = {
+            cursorOuter: "circle-basic",
+            hoverEffect: "pointer-blur",
+            hoverItemMove: false,
+            defaultCursor: false,
+            outerWidth: 41,
+            outerHeight: 41,
+        };
+        magicMouse(options);
+
+        const Cursor = document.getElementById("magicMouseCursor");
+        const Pointer = document.getElementById("magicPointer");
+        if (Cursor != null) {
+            Cursor.style.border = "1px solid #555";
+        }
+        if (Pointer != null) {
+            Pointer.style.background = "#555";
+            Pointer.style.zIndex = "2";
+        }
+        // magicMouse .................................
     }
 });
